012b46b289d3816122747618f4c5fd9e29034694
Wesley Bruno <engwesleybruno@gmail.com>
2022-07-24 10:40:38 -0300

    Modal

---
+++ 1. Layout components/3. Modals/src/App.js
@@ -0,0 +1,40 @@
+import { Modal } from "./Modal";
+import { SmallPersonListItem } from "./people/SmallPersonListItem";
+import { RegularList } from "./RegularList";
+
+const people = [
+  {
+    name: "John Doe",
+    age: 54,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+  {
+    name: "Brenda Smith",
+    age: 33,
+    hairColor: "black",
+    hobbies: ["golf", "mathematics"],
+  },
+  {
+    name: "Jane Garcia",
+    age: 27,
+    hairColor: "blonde",
+    hobbies: ["biology", "medicine", "gymnastics"],
+  },
+];
+
+function App() {
+  return (
+    <>
+      <Modal>
+        <RegularList
+          items={people}
+          resourceName="person"
+          itemComponent={SmallPersonListItem}
+        />
+      </Modal>
+    </>
+  );
+}
+
+export default App;
